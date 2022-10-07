2ca0fbad80c5db8a7e5483b30072c59cf06d0acf
Mjoyvpn <112147124+Mjoyvpn@users.noreply.github.com>
2022-10-07 12:50:52 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -74,7 +74,7 @@ echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 443, 222, 777"  | tee -a log-install.txt
+echo "   - Stunnel4                : 443, 445, 777"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080, 8000 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - OpenVPN OHP             : 8383"  | tee -a log-install.txt
